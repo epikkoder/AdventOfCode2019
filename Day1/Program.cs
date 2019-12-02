35c7583bbf3bd91e3dd3df70416bdf0247bdd8ce
epikkoder <ronaldsahagun87@gmail.com>
2019-12-01 23:23:00 -0800

    Added files

---
+++ Day1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fuelSum = 0;
+            string[] lines = File.ReadAllLines(@"C:\Users\Kenyon\Desktop\input.txt");
+
+            foreach (string line in lines)
+            {
+                fuelSum += GetTotalRequiredFuel(Convert.ToInt32(line));
+            }
+
+            Console.WriteLine(fuelSum);
+        }
+
+        public static int GetRequiredFuel(int module)
+        {
+            return (module / 3) - 2;
+        }
+
+        // For part 2. Needs to account for the fuel also needing fuel.
+        public static int GetTotalRequiredFuel(int module)
+        {
+            int total = 0;
+            int fuel = GetRequiredFuel(module);
+
+            while (fuel > 0)
+            {
+                total += fuel;
+                fuel = GetRequiredFuel(fuel);
+            }
+
+            return total;
+        }
+    }
+}
